4ddb1add61c7960a08103977922bc7e16d6d80c5
Jiyaa17 <132640422+Jiyaa17@users.noreply.github.com>
2024-03-05 20:16:31 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-let text = document.getElementById('text');
-let treeLeft = document.getElementById('tree-left');
-let treeRight = document.getElementById('tree-right');
-let gateLeft = document.getElementById('gate-left');
-let gateRight = document.getElementById('gate-right');
+//selecting eyes
+let eye_ref= document.querySelectorAll(".eye");
 
-window.addEventListener('scroll',() => {
-    let value = window.scrollY;
+//mousemove for laptop/pc and touchmove for mobiles
+let events = ["mousemove","touchmove"];
 
+//check for touch screen
+function isTouchDevice() {
+    try{
+        document.createEvent("TouchEvent");
+        return true;
+    }
+    catch (e) {
+        return false;
+    }
+}
 
-    text.style.marginTop = value * 2.5 + 'px';
-    treeLeft.style.left = value * -1.5 + 'px';
-    treeRight.style.left = value * 1.5 + 'px';
-    gateLeft.style.left = value * 0.5 + 'px';
-    gateRight.style.left = value * -0.5 + 'px';
+events.forEach((eventType) => {
+    document.body.addEventListener(eventType, (event) => {
+        eye_ref.forEach((eye) => {
+            let eyeX = eye.getBoundingClientRect().left + eye.clientWidth /2;
+            let eyeY = eye.getBoundingClientRect().top + eye.clientHeight /2;
 
-});
+            var x = !isTouchDevice() ? event.clientX : event.touches[0].clientX;
+            var y = !isTouchDevice() ? event.clientY : event.touches[0].clientY;
+
+            let radian = Math.atan2(x-eyeX, y-eyeY);
+            let rotationDegrees = radian * ( 180 / Math.PI)* -1 + 180;
+
+            eye.style.transform = "rotate("+rotationDegrees + "deg)";
+        });
+
+        });
+    });
